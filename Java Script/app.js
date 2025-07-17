193400a8e319906cd11584c0a0555780b1dc0539
Abdul Sami <abdulsami42202@gmail.com>
2025-07-17 12:50:08 +0500

    Add renamed folder Sir_Jaffar_Classes

---
+++ Java Script/app.js
@@ -0,0 +1,18 @@
+// alert("WELCOME TO MY WEBSITE");
+// alert("Error! Please enter a valid Password");
+// alert("Welcome to JS Land...\nHappy Coding!");
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+// for (let i = 0; i < 10; i++) {
+//     alert("Happy coding!"+ (i+1));
+    
+// }
+
+var firstName= "Abdul"
+var lastName= "Sami"
+
+alert(`My name is ${firstName} ${lastName}`);
+
+var spec = prompt("your specie","human");
+var numberOfCats= prompt("How many cats?");
+var tooManyCats = numberOfCats+ 1;
