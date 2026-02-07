0eeaf0359d37eb6a1e7b7e0848fdac15c98e751a
Abdul Sami <abdulsami42202@gmail.com>
2026-02-07 15:27:22 +0500

    some changes

--- Java Script/Other Assignments/Forkify Resturant Application/services/fetchRecipeData.js
+++ Java Script/Other Assignments/Forkify Resturant Application/services/fetchRecipeData.js
@@ -5,6 +5,7 @@ const fetchRecipeData = async (recipeId) => {
     const response = await fetch(`${API}/recipes/${recipeId}`);
     const singleRecipeData = await response.json();
     const recipe = singleRecipeData?.data?.recipe;
+    console.log(recipe)
     if (!recipe) {
       console.error("Recipe not found or invalid response", singleRecipeData);
       return;
